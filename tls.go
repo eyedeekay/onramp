a0a35dd81c0d56b34a9dd59dbfce075be8b2d3ac
Haris Khan <hk@hariskhan.dev>
2024-11-04 15:17:48 -0500

    added logging to tls.go

--- tls.go
+++ tls.go
@@ -10,6 +10,7 @@ import (
 	"encoding/asn1"
 	"encoding/pem"
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"math/big"
 	"net"
 	"os"
@@ -24,11 +25,14 @@ import (
 // if no TLS keys exist, they will be generated. They will be valid for
 // the .b32.i2p domain.
 func (g *Garlic) TLSKeys() (tls.Certificate, error) {
+	log.WithField("name", g.getName()).Debug("Getting TLS keys for Garlic service")
 	keys, err := g.Keys()
 	if err != nil {
+		log.WithError(err).Error("Failed to get I2P keys")
 		return tls.Certificate{}, err
 	}
 	base32 := keys.Addr().Base32()
+	log.WithField("base32", base32).Debug("Retrieving TLS certificate for base32 address")
 	return TLSKeys(base32)
 }
 
@@ -36,31 +40,45 @@ func (g *Garlic) TLSKeys() (tls.Certificate, error) {
 // if no TLS keys exist, they will be generated. They will be valid for
 // the .onion domain.
 func (o *Onion) TLSKeys() (tls.Certificate, error) {
+	log.WithField("name", o.getName()).Debug("Getting TLS keys for Onion service")
 	keys, err := o.Keys()
 	if err != nil {
 		return tls.Certificate{}, err
 	}
 	onionService := torutil.OnionServiceIDFromPrivateKey(keys)
+	log.WithField("onion_service", onionService).Debug("Retrieving TLS certificate for onion service")
 	return TLSKeys(onionService)
 }
 
 // TLSKeys returns the TLS certificate and key for the given hostname.
 func TLSKeys(tlsHost string) (tls.Certificate, error) {
+	log.WithField("host", tlsHost).Debug("Getting TLS certificate and key")
 	tlsCert := tlsHost + ".crt"
 	tlsKey := tlsHost + ".pem"
 	if err := CreateTLSCertificate(tlsHost); nil != err {
+		log.WithError(err).Error("Failed to create TLS certificate")
 		return tls.Certificate{}, err
 	}
 	tlsKeystorePath, err := TLSKeystorePath()
 	if err != nil {
+		log.WithError(err).Error("Failed to get TLS keystore path")
 		return tls.Certificate{}, err
 	}
 	tlsCertPath := filepath.Join(tlsKeystorePath, tlsCert)
 	tlsKeyPath := filepath.Join(tlsKeystorePath, tlsKey)
+
+	log.WithFields(logrus.Fields{
+		"cert_path": tlsCertPath,
+		"key_path":  tlsKeyPath,
+	}).Debug("Loading TLS certificate pair")
+
 	cert, err := tls.LoadX509KeyPair(tlsCertPath, tlsKeyPath)
 	if err != nil {
+		log.WithError(err).Error("Failed to load TLS certificate pair")
 		return cert, err
 	}
+
+	log.Debug("Successfully loaded TLS certificate and key")
 	return cert, nil
 }
 
@@ -68,6 +86,7 @@ func TLSKeys(tlsHost string) (tls.Certificate, error) {
 // and stores it in the TLS keystore for the application. If the keys already
 // exist, generation is skipped.
 func CreateTLSCertificate(tlsHost string) error {
+	log.WithField("host", tlsHost).Debug("Creating TLS certificate")
 	tlsCertName := tlsHost + ".crt"
 	tlsKeyName := tlsHost + ".pem"
 	tlsKeystorePath, err := TLSKeystorePath()
@@ -79,70 +98,102 @@ func CreateTLSCertificate(tlsHost string) error {
 	_, certErr := os.Stat(tlsCert)
 	_, keyErr := os.Stat(tlsKey)
 	if certErr != nil || keyErr != nil {
+		log.WithFields(logrus.Fields{
+			"cert_exists": certErr == nil,
+			"key_exists":  keyErr == nil,
+			"cert_path":   tlsCert,
+			"key_path":    tlsKey,
+		}).Debug("Certificate or key missing, generating new ones")
 		if certErr != nil {
+			log.WithField("path", tlsCert).Debug("TLS certificate not found")
 			fmt.Printf("Unable to read TLS certificate '%s'\n", tlsCert)
 		}
 		if keyErr != nil {
+			log.WithField("path", tlsKey).Debug("TLS key not found")
 			fmt.Printf("Unable to read TLS key '%s'\n", tlsKey)
 		}
 
 		if err := createTLSCertificate(tlsHost); nil != err {
+			log.WithError(err).Error("Failed to create TLS certificate")
 			return err
 		}
+	} else {
+		log.Debug("TLS certificate and key already exist")
 	}
 
 	return nil
 }
 
 func createTLSCertificate(host string) error {
+	log.WithField("host", host).Debug("Generating new TLS certificate")
 	fmt.Println("Generating TLS keys. This may take a minute...")
 	priv, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
 	if err != nil {
+		log.WithError(err).Error("Failed to generate private key")
 		return err
 	}
 
 	tlsCert, err := NewTLSCertificate(host, priv)
 	if nil != err {
+		log.WithError(err).Error("Failed to create new TLS certificate")
 		return err
 	}
 	privStore, err := TLSKeystorePath()
 	if nil != err {
+		log.WithError(err).Error("Failed to get keystore path")
 		return err
 	}
 
 	certFile := filepath.Join(privStore, host+".crt")
+	log.WithField("path", certFile).Debug("Saving TLS certificate")
 	// save the TLS certificate
 	certOut, err := os.Create(certFile)
 	if err != nil {
+		log.WithError(err).WithField("path", certFile).Error("Failed to create certificate file")
 		return fmt.Errorf("failed to open %s for writing: %s", host+".crt", err)
 	}
 	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: tlsCert})
 	certOut.Close()
+	log.WithField("path", certFile).Debug("TLS certificate saved successfully")
 	fmt.Printf("\tTLS certificate saved to: %s\n", host+".crt")
 
 	// save the TLS private key
 	privFile := filepath.Join(privStore, host+".pem")
+	log.WithField("path", privFile).Debug("Saving TLS private key")
 	keyOut, err := os.OpenFile(privFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
+		log.WithError(err).WithField("path", privFile).Error("Failed to create private key file")
 		return fmt.Errorf("failed to open %s for writing: %v", privFile, err)
 	}
 	secp384r1, err := asn1.Marshal(asn1.ObjectIdentifier{1, 3, 132, 0, 34}) // http://www.ietf.org/rfc/rfc5480.txt
+	if err != nil {
+		log.WithError(err).Error("Failed to marshal EC parameters")
+		return err
+	}
 	pem.Encode(keyOut, &pem.Block{Type: "EC PARAMETERS", Bytes: secp384r1})
 	ecder, err := x509.MarshalECPrivateKey(priv)
+	if err != nil {
+		log.WithError(err).Error("Failed to marshal private key")
+		return err
+	}
 	pem.Encode(keyOut, &pem.Block{Type: "EC PRIVATE KEY", Bytes: ecder})
 	pem.Encode(keyOut, &pem.Block{Type: "CERTIFICATE", Bytes: tlsCert})
 
 	keyOut.Close()
+	log.WithField("path", privFile).Debug("TLS private key saved successfully")
 	fmt.Printf("\tTLS private key saved to: %s\n", privFile)
 
 	// CRL
 	crlFile := filepath.Join(privStore, host+".crl")
+	log.WithField("path", crlFile).Debug("Creating CRL")
 	crlOut, err := os.OpenFile(crlFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
+		log.WithError(err).WithField("path", crlFile).Error("Failed to create CRL file")
 		return fmt.Errorf("failed to open %s for writing: %s", crlFile, err)
 	}
 	crlcert, err := x509.ParseCertificate(tlsCert)
 	if err != nil {
+		log.WithError(err).Error("Failed to parse certificate for CRL creation")
 		return fmt.Errorf("Certificate with unknown critical extension was not parsed: %s", err)
 	}
 
@@ -156,10 +207,12 @@ func createTLSCertificate(host string) error {
 
 	crlBytes, err := crlcert.CreateCRL(rand.Reader, priv, revokedCerts, now, now)
 	if err != nil {
+		log.WithError(err).Error("Failed to create CRL")
 		return fmt.Errorf("error creating CRL: %s", err)
 	}
 	_, err = x509.ParseDERCRL(crlBytes)
 	if err != nil {
+		log.WithError(err).Error("Failed to validate generated CRL")
 		return fmt.Errorf("error reparsing CRL: %s", err)
 	}
 	pem.Encode(crlOut, &pem.Block{Type: "X509 CRL", Bytes: crlBytes})
